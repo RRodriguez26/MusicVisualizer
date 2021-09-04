28912ca01d94817ffe4cd72475ac00696d225260
Rafael Perez <rafaelandresph@gmail.com>
2021-09-04 00:59:50 -0400

    add mp3 file and add 3 functions to play music

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,23 @@
+var song;
+
+function preload() {
+  song = loadSound('../hideaway(Mitch).mp3');
+}
+
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   // put drawing code here
+  background(0);
+}
+
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
 }
